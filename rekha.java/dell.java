aa3218d24587ec516407c0e401f67e94ba6ffd56
Vachan1010 <rekhachitra123@gmail.com>
2025-06-19 09:32:54 +0000

    Added sonar config

--- rekha.java/dell.java
+++ rekha.java/dell.java
@@ -1,6 +1,18 @@
+<<<<<<< HEAD
 welcome to git hub
 jenkins build now  
 hello
 hi
 welcome to devops
 
+=======
+HEAD
+hi welcome to git hub
+jenkins build now  
+
+hello
+hi
+update 
+add
+>>>>>>> 89eb6d5 (Added sonar config)
+>>>>>>> b2338a0 (Added sonar config)
